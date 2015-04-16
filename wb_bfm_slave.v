12507b9c38fb76c9eb52707c3202daca39555c6e
Olof Kindgren <olof.kindgren@gmail.com>
2015-04-16 13:18:13 +0200

    Add license headers

--- wb_bfm_slave.v
+++ wb_bfm_slave.v
@@ -1,3 +1,22 @@
+/*
+* Wishbone Slave BFM
+* Copyright (C) 2013-2015 Olof Kindgren <olof.kindgren@gmail.com>
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
 module wb_bfm_slave
   #(parameter dw = 32,
     parameter aw = 32,
